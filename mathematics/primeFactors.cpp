f2f1f99cd929ea8f63010e86e0869b5fc5f91140
trX-X <tarinkumarpodavu4@gmail.com>
2021-10-14 18:32:04 +0530

    Problems on Prime Day 3

---
+++ mathematics/primeFactors.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+bool isPrime(int n){
+    if(n == 1) return false;
+    if((n == 2) || (n == 3)) return true;
+    if((n%2 == 0) || (n%3 == 0)) return false;
+    for(int i=5; i*i <= n; i=i+6){
+        if((n%i == 0) || (n%(i+2) == 0)) return false;
+    }   
+    return true;
+}
+
+//naive solution
+// void primeFactors(int n){
+//     for(int i = 2; i <= n; i++){
+//         if(isPrime(i)){
+//             int x = i;
+//             while(n % x == 0){
+//                 cout<<i;
+//                 x = x * i;
+//             }
+//         }
+//     }
+// }
+
+//Efficient solution
+void primeFactors(int n){
+    if(n <= 1) return;
+    for(int i=2; i*i<=n; i++){
+        while(n%i == 0){
+            cout<<i<<" ";
+            n = n/i;
+        }
+    }
+    if(n>1){
+            cout<<n<<" ";
+    }
+}
+
+
+int main(void){
+    primeFactors(450);
+    return 0;
+}
